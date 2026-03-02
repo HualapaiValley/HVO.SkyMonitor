f03d9392c0c404775b5414f729e4cb5ef582f7b9
Roy Salisbury <roys@express-is.net>
2026-03-02 04:09:15 +0000

    refactor: replace HVO.Common project references with HVO.Core NuGet package
    - Add HVO.Core 1.0.0 to Directory.Packages.props
    - Replace HVO.Common ProjectReference with HVO.Core PackageReference in
      HVO.SkyMonitor.Common and HVO.SkyMonitor.CameraAgent.Common
    - Remove redundant HVO.Common ProjectReference from CameraAgent,
      HVO.SkyMonitor.csproj, and HVO.SkyMonitor.LogicHost.csproj (transitive)
    - Update SkiaPreviewEncoder.cs: using HVO -> using HVO.Core.Results
    - Remove HVO.Common.Tests project (types now tested in HVO.Core)
    - Keep HVO.Common project until AstronomyMath and AttributeExtensions
      are migrated to HVO.Core

--- src/HVO.SkyMonitor.CameraAgent.Common/Imaging/SkiaPreviewEncoder.cs
+++ src/HVO.SkyMonitor.CameraAgent.Common/Imaging/SkiaPreviewEncoder.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
-using HVO;
+using HVO.Core.Results;
 using SkiaSharp;
 
 namespace HVO.SkyMonitor.CameraAgent.Common.Imaging;
