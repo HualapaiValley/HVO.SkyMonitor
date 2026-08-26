9c7954c8d9133b5806cade3edbf5404c0eb53f7a
Roy Salisbury <roy@express-is.net>
2026-08-26 19:05:59 +0000

    test: bind corrected layered evidence

--- tests/HVO.SkyMonitor.IntegrationTests/LogicHostLayeredPerformanceTests.cs
+++ tests/HVO.SkyMonitor.IntegrationTests/LogicHostLayeredPerformanceTests.cs
@@ -29,7 +29,7 @@ namespace HVO.SkyMonitor.IntegrationTests;
 public sealed partial class LogicHostIngestPerformanceTests
 {
     private const string Issues437432Baseline = "5febc8efaa4b4b6e5e45eb45e9df0c55525bfdde";
-    private const string Issues437432Product = "06908f96db644ba6d2833e4eccdba76dfbda1e59";
+    private const string Issues437432Product = "2e834fa765cc27bea4888aedde5a0b38eee9ffba";
     private const string Issues437432ResultSchema = "issues-437-432-layered-performance-v2";
     private const int Issues437432Width = 3096;
     private const int Issues437432Height = 2080;
