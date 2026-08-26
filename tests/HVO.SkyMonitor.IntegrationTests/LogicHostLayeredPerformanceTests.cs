1293f85b23310726c0826c36a6d0bdc05390e23b
Roy Salisbury <roy@express-is.net>
2026-08-26 22:29:05 +0000

    test: rebind final layered evidence

--- tests/HVO.SkyMonitor.IntegrationTests/LogicHostLayeredPerformanceTests.cs
+++ tests/HVO.SkyMonitor.IntegrationTests/LogicHostLayeredPerformanceTests.cs
@@ -29,7 +29,7 @@ namespace HVO.SkyMonitor.IntegrationTests;
 public sealed partial class LogicHostIngestPerformanceTests
 {
     private const string Issues437432Baseline = "5febc8efaa4b4b6e5e45eb45e9df0c55525bfdde";
-    private const string Issues437432Product = "a5e9c38de85d265c8f10b4149d9fa854b2c2d6f2";
+    private const string Issues437432Product = "9b051a38aa58f06d01b6951239333d0127634f1e";
     private const string Issues437432ResultSchema = "issues-437-432-layered-performance-v2";
     private const int Issues437432Width = 3096;
     private const int Issues437432Height = 2080;
